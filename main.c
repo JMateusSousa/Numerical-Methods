934b2ac38f04caae443b6dc814fa3dbdb1c23457
Victor Luca <vluca002@gmail.com>
2019-04-09 21:12:24 -0300

    Alterando conversão

--- main.c
+++ main.c
@@ -1,88 +1,146 @@
 #include <stdio.h>
 
 void converteBin(double decimal) {    //Converte o número decimal para binário.
-    int inteiro, i = 0;
+    int inteiro, i = 0, j;
     int aux[20];
     double fracionario;
 
     inteiro = (int) decimal;    //Pega a somente a parte inteira do decimal.
     fracionario = decimal - inteiro;
 
-    if(inteiro > 0){    //verifica se a parte inteira é maior que 0.
-        while (inteiro != 0 && i <= 20) {
-            aux[i] = inteiro % 2;
-            inteiro /= 2;
-            i++;
-        }
+    if(inteiro == 0) printf("0");	//Caso a parte inteira seja 0.
 
-        for (int j = i - 1; j >= 0; j--) {
-            printf("%d", aux[j]);   //Imprime a parte inteira do número binário.
-        }
+    while (inteiro != 0 && i <= 20) {
+        aux[i] = inteiro % 2;
+        inteiro /= 2;
+        i++;
     }
-    else printf("0");
+
+    for (j = i - 1; j >= 0; j--) {
+        printf("%d", aux[j]);   //Imprime a parte inteira do número binário.
+    }
 
     i = 0;
+    if(fracionario != 0.0) printf(".");
 
-    if (fracionario > 0.0) {    //Verifica se existe parte fracionária.
-        printf(".");
+    while (fracionario != 0.0 && i <= 20) {
+        fracionario *= 2;
 
-        while (fracionario != 0.0 && i <= 20) {
-            fracionario *= 2;
+        if (fracionario >= 1) {   //Caso o resultado da multiplicação seja maior que 1 imprime-se 1, caso contrário 0.
+            printf("%d", 1);
+            fracionario -= 1;   //Retirando a parte inteira.
 
-            if (fracionario >=
-                1) {   //Caso o resultado da multiplicação seja maior que 1 imprime-se 1, caso contrário 0.
-                printf("%d", 1);
-                fracionario -= 1;   //Retirando a parte inteira.
-
-            } else printf("%d", 0);
-        }
+        } else printf("%d", 0);
     }
+
+    printf("\n");
+
 }
 
 void converteOct(double decimal) {    //Converte o número decimal para Octal.
-    int inteiro, i = 0;
+    int inteiro, i = 0, j;
     int aux[20];
     double fracionario;
 
     inteiro = (int) decimal;    //Pega a somente a parte inteira do decimal.
     fracionario = decimal - inteiro;
 
-    if(inteiro > 0){    //verifica se a parte inteira é maior que 0.
-        while (inteiro != 0 && i <= 20) {
-            aux[i] = inteiro % 8;
-            inteiro /= 8;
-            i++;
-        }
+    if(inteiro == 0) printf("0");	//Caso a parte inteira seja 0.
 
-        for (int j = i - 1; j >= 0; j--) {
-            printf("%d", aux[j]);   //Imprime a parte inteira do número binário.
-        }
+    while (inteiro != 0 && i <= 20) {
+        aux[i] = inteiro % 8;
+        inteiro /= 8;
+        i++;
     }
-    else printf("0");
+
+    for (j = i - 1; j >= 0; j--) {
+        printf("%d", aux[j]);   //Imprime a parte inteira do número binário.
+    }
 
     i = 0;
+    if(fracionario != 0.0) printf(".");
 
-    if (fracionario > 0.0) {    //Verifica se existe parte fracionária.
-        printf(".");
+    while (fracionario != 0.0 && i <= 20) {
+        fracionario *= 8;
 
-        while (fracionario != 0.0 && i <= 20) {
-            fracionario *= 8;
+        if (fracionario > 1) {   //Caso o resultado da multiplicação seja maior que 1 imprime-se a parte inteira, caso contrário 0.
+            printf("%d", (int) fracionario);
+            fracionario -= (int) fracionario;   //Retirando a parte inteira.
 
-            if (fracionario > 1) {   //Caso o resultado da multiplicação seja maior que 8 imprime-se a parte inteira, caso contrário 0.
-                printf("%d", (int) fracionario);
-                fracionario -= (int) fracionario;   //Retirando a parte inteira.
-
-            } else printf("%d", 0);
-        }
+        } else printf("%d", 0);
     }
+    printf("\n");
 }
 
-int main() {
+void converteHex(double decimal) {    //Converte o número decimal para Hexadecimal.
+    int inteiro, i = 0, j;
+    int aux[20];
+    double fracionario;
+
+    inteiro = (int) decimal;    //Pega a somente a parte inteira do decimal.
+    fracionario = decimal - inteiro;
+
+    if(inteiro == 0) printf("0");	//Caso a parte inteira seja 0.
+
+    while (inteiro != 0 && i <= 20) {
+        aux[i] = inteiro % 16;
+        inteiro /= 16;
+        i++;
+
+    }
+
+    for (j = i - 1; j >= 0; j--) {
+        if(aux[j] >= 10){
+            printf("%c", aux[j] + 55);	//Printando o caractere respectivo ao numero hexadecimal.
+        }
+        else printf("%d", aux[j]);
+    }
+
+    i = 0;
+    if(fracionario != 0.0) printf(".");
+
+    while (fracionario != 0.0 && i <= 20) {
+        fracionario *= 16;
+
+        if (fracionario > 1) {   //Caso o resultado da multiplicação seja maior que 1 imprime-se a parte inteira, caso contrário 0.
+            if(fracionario >= 10){
+                printf("%c", (int)fracionario + 55);
+            }
+            else printf("%d", (int)fracionario);
+
+            fracionario -= (int) fracionario;   //Retirando a parte inteira.
+
+        } else printf("%d", 0);
+    }
+    printf("\n");
+}
+
+void conversao(){
     double decimal;
 
     printf("Digite o numero decimal: ");
     scanf("%lf", &decimal);
 
+    printf("Binario: ");
+    converteBin(decimal);
+    printf("Octal: ");
     converteOct(decimal);
+    printf("Hexadecimal: ");
+    converteHex(decimal);
+}
+
+int main() {
+    char opcao;
+
+    printf("Digite C para realizar a conversao: ");
+    scanf("%c", &opcao);
+
+    switch(opcao){
+        case 'C':
+            conversao();
+            break;
+    }
+
     return 0;
 }
+
